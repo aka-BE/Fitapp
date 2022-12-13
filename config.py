c224d7784166e532f164e264a99689722a75c028
Bika <eradzeb@proton.me>
2022-12-13 21:24:38 +0400

    Config

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
 
     # General Config
     FLASK_APP = environ.get('FLASK_APP')
-    FLASK_ENV = environ.get('FLASK_ENV')
+    FLASK_DEBUG = environ.get('FLASK_DEBUG')
     SECRET_KEY = environ.get('SECRET_KEY')
     TESTING = True
     STATIC_FOLDER = 'static'
